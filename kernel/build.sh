31fb39f9c50fcf1c3e376ac9ec8fceeb6b24ff37
lingfude <ling.fude@gmail.com>
2014-05-19 21:30:09 +0100

    Rename generated kernelfile to kernel

--- kernel/build.sh
+++ kernel/build.sh
@@ -184,7 +184,7 @@ echo "**** Successfully built kernel ****"
 
 echo "**** Copying kernel to /build_result/kernel/ ****"
 mkdir -p ../build_result/kernel/
-cp kernelFile ../build_result/kernel/
+cp kernelFile ../build_result/kernel/kernel
 
 echo "**** Copying all built modules (.ko) to /build_result/modules/ ****"
 mkdir -p ../build_result/modules/
