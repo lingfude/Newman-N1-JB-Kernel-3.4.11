d1d048897dcd9a69781023c8b47420d92e3df99f
dragonpt <cesar.maximo@gmail.com>
2014-06-01 09:45:56 +0100

    CPU Governors: Added SmartAssV2

--- kernel/include/linux/cpufreq.h
+++ kernel/include/linux/cpufreq.h
@@ -382,6 +382,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSH3)
 extern struct cpufreq_governor cpufreq_gov_smartassH3;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartassH3)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR (&amp;cpufreq_gov_smartass2)
 #endif
 
 
