a72ceef505c56f678bd0e88f5d73d110b2752058
lingfude <ling.fude@gmail.com>
2014-06-01 23:35:32 +0100

    ToolChain: Updated to Linaro Toolchain 4.9.1 Optimized Cortex a9

--- kernel/build.sh
+++ kernel/build.sh
@@ -9,7 +9,7 @@ makeflags="-w -j8"
 makedefs="V=0"
 makejobs=${MAKEJOBS}
 curdir=`pwd`
-TOOLCHAIN="./toolchain/Arm_A9_linaro_4.8.3/bin"
+TOOLCHAIN="./toolchain/arm-cortex_a9-linux-gnueabihf-linaro_4.9.1-2014.05/bin"
 #if [ "${KBUILD_OUTPUT_SUPPORT}" == "yes" ];then
 #  outdir=$curdir/out
 #  mkdir -p $outdir
@@ -200,7 +200,7 @@ done
 
 echo "**** Patching all built modules (.ko) in /build_result/modules/ ****"
 cd ..
-find ./build_result/modules/ -type f -name '*.ko' | xargs -n 1 $TOOLCHAIN/arm-gnueabi-strip --strip-unneeded
+find ./build_result/modules/ -type f -name '*.ko' | xargs -n 1 $TOOLCHAIN/arm-eabi-strip --strip-unneeded
 echo "**** Finnish ****"
 
 echo "**** You can find kernelFile in root folder: /build_result/kernel/ ****"
